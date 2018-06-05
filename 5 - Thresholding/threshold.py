53dfa327ddf263f2c7980c6d30b9f5ba62521b8e
Hunter Whaples <hwhaples@g.hmc.edu>
2018-06-05 01:39:42 -0400

    completed thresholding

---
+++ 5 - Thresholding/threshold.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as numpy
+#this program wil use thresholding to make an image of a bookpage more legible
+#threshold, grayscaling, gaussian, and otsu are all openCV methods.
+
+img = cv2.imread('bookpage.jpg')
+retval, threshold = cv2.threshold(img, 12, 255, cv2.THRESH_BINARY) #12 becomes lower threshold
+
+grayscaled = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+retval2, threshold2 = cv2.threshold(grayscaled, 12, 255, cv2.THRESH_BINARY)
+
+gaus = cv2.adaptiveThreshold(grayscaled, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 155, 1)
+
+retval2,otsu = cv2.threshold(grayscaled, 125, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+
+
+cv2.imshow('original', img)
+cv2.imshow('threshold', threshold)
+cv2.imshow('threshold2', threshold2)
+cv2.imshow('adaptive', gaus)
+cv2.imshow('otsu', otsu)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
